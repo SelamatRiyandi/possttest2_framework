346320befcc5ebec1123995374a9cfc99e5e4dbf
riyandiii <iniemailriyandi@gmail.com>
2023-10-11 07:23:39 +0800

    ionix

--- app/Http/Controllers/klien.php
+++ app/Http/Controllers/klien.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Model\klien;
 
-class klien extends Controller
+class controllerklien extends Controller
 {
     /**
      * Display a listing of the resource.
--- resources/views/client.blade.php
+++ resources/views/client.blade.php
@@ -8,62 +8,10 @@
         Kami ingin mengucapkan terima kasih yang tulus kepada semua klien kami yang telah memilih Ionix sebagai mitra dalam menjaga kesehatan mereka. Setiap nama dalam daftar ini adalah cerminan dari kepercayaan yang diberikan kepada kami untuk memberikan perlindungan kesehatan yang berkualitas tinggi, pelayanan yang berorientasi pada kebutuhan, dan komitmen kami untuk selalu berada di samping Anda dalam setiap langkah perjalanan kesehatan Anda.
     </p>
 </div>
-    {{-- <table class="ctable">
-        <thead style="border-radius: 10px">
-            <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Nama</th>
-                <th scope="col">Tanggal Lahir</th>
-                <th scope="col">Kategori</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <th scope="row">2109116064</th>
-                <td>Riyandi</td>
-                <td>26/06/03</td>
-                <td>Kategori I</td>
-            </tr>
-            <tr>
-                <th scope="row">2109116084</th>
-                <td>Indra</td>
-                <td>07/03/03</td>
-                <td>Kategori II</td>
-            </tr>
-            <tr>
-                <th scope="row">2109116059</th>
-                <td>Raya</td>
-                <td>27/06/03</td>
-                <td>Kategori III</td>
-            </tr>
-            <tr>
-                <th scope="row">2109116068</th>
-                <td>Wahyu</td>
-                <td>25/08/03</td>
-                <td>Kategori II</td>
-            </tr>
-            <tr>
-                <th scope="row">2109116038</th>
-                <td>Daiva</td>
-                <td>22/07/04</td>
-                <td>Kategori I</td>
-            </tr>
-            <tr>
-                <th scope="row">2109116058</th>
-                <td>Firzian</td>
-                <td>13/06/03</td>
-                <td>Kategori II</td>
-            </tr>
-            <tr>
-                <th scope="row">2109116045</th>
-                <td>Wibi</td>
-                <td>09/09/03</td>
-                <td>Kategori III</td>
-            </tr>
-        </tbody>
-
-    </table> --}}
-    <table class="ctable">
+<a href = "/tambah-post">+ Tambah File Post </a>
+<br>
+<br>
+<table class="ctable">
     <thead style="border-radius: 10px">
         <tr>
             <th></th>
@@ -88,5 +36,5 @@
 
         </tr>
     </tbody>
-    {{-- </div> --}}
+</table>
 @endsection
